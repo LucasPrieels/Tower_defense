586075c7ceb22920e2e86546841ea3b68f469ae3
Lucas Prieels <lucas.prieels@ulb.be>
2020-04-14 21:29:47 +0200

    Create Tower class

--- src/Model/Tower.java
+++ src/Model/Tower.java
@@ -1,7 +1,13 @@
 package Model;
 
-public class Tower {
-    private static double range;
-    public Tower(){
+public abstract class Tower {
+    private int upgrade_level;
+    private Asteroid asteroid;
+    public Tower(Asteroid asteroid){
+        this.asteroid = asteroid;
+        upgrade_level = 1;
     }
+    public abstract void upgrade();
+
+    public abstract void delete();
 }
