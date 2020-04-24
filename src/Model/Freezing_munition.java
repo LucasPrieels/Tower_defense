72da5b2a58f9a01f7656aa1fc3aba835a17ad491
Walid022 <63810108+Walid022@users.noreply.github.com>
2020-04-24 13:57:53 +0200

    Freezing_munition

--- src/Model/Freezing_munition.java
+++ src/Model/Freezing_munition.java
@@ -1,7 +1,12 @@
 package Model;
 
 public class Freezing_munition extends Munition {
-    public Freezing_munition(Tower tower, NPC npc){
+
+    private int freezing_time;
+
+    public Freezing_munition(Tower tower, NPC npc, int freezing_time){
+
         super(tower, npc);
+        this.freezing_time=freezing_time;
     }
 }
