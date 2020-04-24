b5dd2379db6b9510794f52944ab57493325d2523
Walid022 <63810108+Walid022@users.noreply.github.com>
2020-04-24 13:48:17 +0200

    Munition

--- src/Model/Munition.java
+++ src/Model/Munition.java
@@ -1,9 +1,15 @@
 package Model;
 
-public abstract class Munition {
+public  class Munition {
+
     private Tower tower;
     private NPC npc;
     private int pos_x, pos_y;
+    private boolean hit = false;
+    private int endpos_x,endpos_y;
+// faut amener le projectile sur le npc
+//de facon à que  les coordonées finals du projectile soit  endpos_x et endpos_y
+
 
     public Munition(Tower tower, NPC npc){
         this.tower = tower;
@@ -12,6 +18,18 @@ public abstract class Munition {
         this.pos_y = tower.get_asteroid().get_pos_y();
     }
 
+    public boolean Hit_npc(NPC npc){
+        if (endpos_x == npc.get_pos_x() && endpos_y == npc.get_pos_y()  ){
+            hit = true;
+        }
+        return hit;
+
+    }
+
+    public NPC get_NPC(){return npc;}
+
     public int get_pos_x(){return pos_x;}
     public int get_pos_y(){return pos_y;}
+
 }
+
