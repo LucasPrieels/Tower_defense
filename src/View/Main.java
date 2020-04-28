181a74edb46f2cf04001d2fa4659c582e66a2f73
Walid022 <63810108+Walid022@users.noreply.github.com>
2020-04-28 20:36:31 +0200

    Change2

--- src/View/Main.java
+++ src/View/Main.java
@@ -9,7 +9,30 @@ import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
 
-public class Main  {
+
+
+
+
+public class Main extends Application  {
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+        Menu menu = new Menu();
+        primaryStage =  menu.getMainStage();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Tower defense");
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
     /*
 
     private static Stage theStage;
