fe27440d92dcf3620fd560f2be2e9a78bad3d2c5
Walid022 <63810108+Walid022@users.noreply.github.com>
2020-05-02 15:56:29 +0200

    change

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -56,9 +56,8 @@ public class Menu extends Parent {
     private Button button_level;
     private Button button_exit;
 
-    private VBox vbox;
-    private VBox vbox_rules;
-    private  VBox root;
+    private Group vbox;
+
 
 
     private  static String texte;
@@ -69,6 +68,8 @@ public class Menu extends Parent {
     private static String text;
     private Label label_help;
     private BorderPane canvasBorderPane;
+
+
 
     //play
     private Group root_play;
@@ -122,27 +123,10 @@ public class Menu extends Parent {
         button_exit.setStyle("-fx-background-color:  #ffd700");
 
 
-        //buttons
 
-        vbox = new VBox();
-        vbox.setLayoutX(400);
-        vbox.setLayoutY(100);
+        vbox = new Group();
 
-
-        vbox_rules = new VBox();
-        vbox_rules.setLayoutX(400);
-        vbox_rules.setLayoutY(100);
-
-
-
-        root = new VBox();
-        root.setLayoutX(400);
-        root.setLayoutY(300);
-        root.setPrefSize(400,200);
-
-
-
-        this.getChildren().addAll(imageView,button_rules,button_help,button_level,button_exit,vbox,root,vbox_rules);
+        this.getChildren().addAll(imageView,button_rules,button_help,button_level,button_exit,vbox);
 
         button_exit.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -156,11 +140,13 @@ public class Menu extends Parent {
             @Override
             public void handle(ActionEvent actionEvent) {
                 vbox.getChildren().clear();
-                vbox_rules.getChildren().clear();
-                root.getChildren().clear();
-                 texte =  "Tower Defense :\n\n"+"Player start with an appropriate amount of currency and is able to obtain more money during game by defeating waves of  enemies and also by building a Factory tower."+"There are\n 3  waves on each level  and  each wave of enemies spawns with a short pause\n in between waves.\n\n The player  buy towers through currency pieces and than he should  place the tower\n on a asteroid location." +
-                        " Towers can be upgraded at least three times to have more range, or damage.\n \n" +
-                        " The player win the  game when his score is higher than  0 or lose   when his \n score is  0.\n" +
+                vbox.setLayoutX(400);
+                vbox.setLayoutY(100);
+
+
+                 texte =  "Tower Defense :\n\n"+"Player start with an appropriate amount of currency and is able to obtain\n more money during game by defeating waves of  enemies and also by\n building a Factory tower."+"There are 3  waves on each level  and  each wave\n of enemies spawns with a short pause in between waves.\n\n The player  buy towers through currency pieces and than he should place\n the tower on a asteroid location." +
+                        " Towers can be upgraded at least three\ntimes to have more range, or damage.\n \n" +
+                        " The player win the  game when his score is higher than  0 or lose  when his\n  score is  0.\n" +
                         "\n"+"\n"+"\n"+"\n";
 
                  label = new Label(texte);
@@ -173,10 +159,11 @@ public class Menu extends Parent {
                 canvas.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
                 canvas.setCenter(label);
 
-                vbox_rules.setVisible(true);
 
-                vbox_rules.getChildren().add(canvas);
-                vbox_rules.setPrefSize(800,800);
+                vbox.setVisible(true);
+
+
+                vbox.getChildren().add(canvas);
 
 
             }
@@ -187,8 +174,10 @@ public class Menu extends Parent {
                                     @Override
                                     public void handle(ActionEvent actionEvent) {
                                             vbox.getChildren().clear();
-                                        vbox_rules.getChildren().clear();
-                                        root.getChildren().clear();
+                                            vbox.setLayoutX(400);
+                                            vbox.setLayoutY(100);
+
+
                                          text = "HELP:\n\n"+"Press the  play button to start the game.\n\n" +
                                                 "Change the difficulty on level mode tab.\n\n" +
                                                 "Press the exit button if you want to quit the game."
@@ -197,11 +186,11 @@ public class Menu extends Parent {
                                                 + "                 ";
                                          label_help = new Label(text);
                                         label_help.setFont(Font.font("Cambria", 20));
-                                        label_help.setTextFill(Color.web("#DC143C"));
+                                        label_help.setTextFill(Color.web("#000000"));
                                         label_help.setWrapText(true);
 
 
-                                        //BorderPane canvasBorderPane = new BorderPane();
+
                                         canvasBorderPane = new BorderPane();
                                         canvasBorderPane.setPadding(new Insets(10));
                                         canvasBorderPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
@@ -210,7 +199,7 @@ public class Menu extends Parent {
 
                                         vbox.setVisible(true);
                                         vbox.getChildren().add(canvasBorderPane);
-                                        vbox.setPrefSize(540,410);
+
 
 
                                     }
@@ -229,8 +218,9 @@ public class Menu extends Parent {
 
 
                 vbox.getChildren().clear();
-                vbox_rules.getChildren().clear();
-                root.getChildren().clear();
+                vbox.setLayoutX(400);
+                vbox.setLayoutY(300);
+
 
                 HBox menulevels = new HBox(10);
                 menulevels.setPadding(new Insets(15));
@@ -240,7 +230,8 @@ public class Menu extends Parent {
                 label_level.setTextFill(Color.web("#000000"));
                 menulevels.getChildren().add(label_level);
 
-                root.setVisible(true);
+
+                vbox.setVisible(true);
                 level1  = new Button("Level 1");
                 level2  = new Button("Level 2");
                 level3  = new Button("Level 3");
@@ -255,7 +246,8 @@ public class Menu extends Parent {
                 canvasBorderPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
                 canvasBorderPane.setCenter(menulevels);
 
-                root.getChildren().add(canvasBorderPane);
+                //root.getChildren().add(canvasBorderPane);
+                vbox.getChildren().add(canvasBorderPane);
 
                 level1.setOnMouseClicked(new EventHandler<MouseEvent>() {
                     @Override
