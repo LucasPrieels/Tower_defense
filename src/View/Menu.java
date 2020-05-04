0fb9e843cb58bb08364ae2cbdd626ea7450f51f9
Lucas Prieels <lucas.prieels@ulb.be>
2020-05-05 01:15:56 +0200

    Added an error message if not saved game is found and few improvements on the code of Menu

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -28,76 +28,30 @@ import java.io.*;
 
 public class Menu extends Parent implements Serializable{
     private Stage theStage;
-    private Button level1, level2, level3, button_rules, button_help, button_start, button_exit, button_load;
     private Image image;
-    private ImageView imageView;
-    private Group vbox;
-    //rules
-    private Label label;
-    private BorderPane canvas;
-    //help
-    private static String text;
-    private Label label_help;
-    private BorderPane canvasBorderPane;
-    //play
-    private Group root_play;
     private int level = 1; // Niveau sélectionné si on n'en sélectionne pas d'autre
-    // COLOR
-    BorderPane border;
+
     public Menu(Stage theStage){
         try{
             image = new Image(new FileInputStream("Images/menu.jpg"));
         } catch(FileNotFoundException e){
             e.printStackTrace();
         }
-        imageView = new ImageView(image);
+        ImageView imageView = new ImageView(image);
         this.theStage = theStage;
 
-        button_start = new Button("Start");
-        button_start.setLayoutX(100);
-        button_start.setLayoutY(100);
-        button_start.setPrefWidth(120);
-        button_start.setPrefHeight(30);
-        button_start.setStyle("-fx-background-color:  #ffffff");
+        Button button_start = new_button("Start", 100, 100, 120, 30, "-fx-background-color:  #ffffff");
+        Button button_rules = new_button("Rules", 100, 200, 120, 30, "-fx-background-color:  #ffffff");
+        Button button_load = new_button("Load last game", 100, 300, 120, 30, "-fx-background-color:  #ffffff");
+        Button button_help = new_button("Help", 100, 400, 120, 30, "-fx-background-color:  #ffffff");
+        Button button_exit = new_button("Exit", 100, 500, 120, 30, "-fx-background-color:  #ffffff");
 
-        button_rules = new Button("Rules");
-        button_rules.setLayoutX(100);
-        button_rules.setLayoutY(200);
-        button_rules.setPrefWidth(120);
-        button_rules.setPrefHeight(30);
-        button_rules.setStyle("-fx-background-color:  #ffffff");
-
-        button_load = new Button("Load last game");
-        button_load.setLayoutX(100);
-        button_load.setLayoutY(300);
-        button_load.setPrefWidth(120);
-        button_load.setPrefHeight(30);
-        button_load.setStyle("-fx-background-color:  #ffffff");
-         
-        button_help = new Button("Help");
-        button_help.setLayoutX(100);
-        button_help.setLayoutY(400);
-        button_help.setPrefWidth(120);
-        button_help.setPrefHeight(30);
-        button_help.setStyle("-fx-background-color:  #ffffff");
-         
-        button_exit = new Button("Exit");
-        button_exit.setLayoutX(100);
-        button_exit.setLayoutY(500);
-        button_exit.setPrefWidth(120);
-        button_exit.setPrefHeight(30);
-        button_exit.setStyle("-fx-background-color:  #ffffff");
-
-        border = new BorderPane();
-        border.setPadding(new Insets(5));
-        border.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(0), Insets.EMPTY)));
-         
-        vbox = new Group();
+        Group vbox = new Group();
         this.getChildren().addAll(imageView, button_rules, button_help, button_start, button_exit, button_load, vbox);
          
         button_exit.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent mouseEvent) {
+            public void handle(MouseEvent mouseEvent){
                 theStage.close();
             }
         });
@@ -105,57 +59,24 @@ public class Menu extends Parent implements Serializable{
         button_rules.setOnAction(new EventHandler<ActionEvent>() {
             @Override 
             public void handle(ActionEvent actionEvent) {
-                vbox.getChildren().clear();
-                vbox.setLayoutX(400);
-                vbox.setLayoutY(100);
                 String text =  "Tower Defense :\n\n"+"Player start with an appropriate amount of currency and is able to obtain\n more money during game by defeating waves of  enemies and also by\n building a Factory tower."+"There are 3  waves on each level  and  each wave\n of enemies spawns with a short pause in between waves.\n\n The player  buy towers through currency pieces and than he should place\n the tower on a asteroid location." +
                         " Towers can be upgraded at least three\ntimes to have more range, or damage.\n \n" +
                         " The player win the  game when his score is higher than  0 or lose  when his\n  score is  0.\n" +
                         "\n"+"\n"+"\n"+"\n";
-
-                label = new Label(text);
-                label.setFont(Font.font("Cambria", 20));
-                label.setTextFill(Color.web("#000000"));
-                label.setWrapText(true);
-
-                canvas = new BorderPane();
-                canvas.setPadding(new Insets(5));
-                canvas.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
-                canvas.setCenter(label);
-
-                border.getChildren().clear();
-                border.setCenter(canvas);
-                vbox.getChildren().add(border); 
+                vbox_text(vbox, text);
             }
         });
          
         button_help.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                vbox.getChildren().clear();
-                vbox.setLayoutX(400);
-                vbox.setLayoutY(100);
-                
-                text = "HELP:\n\n"+"Press the  play button to start the game.\n\n" +
+                String text = "HELP:\n\n"+"Press the  play button to start the game.\n\n" +
                         "Change the difficulty on level mode tab.\n\n" +
                         "Press the exit button if you want to quit the game."
                         +"                                                  \n\n "
                         + "                                                 \n\n"
                         + "                 ";
-                label_help = new Label(text);
-                label_help.setFont(Font.font("Cambria", 20));
-                label_help.setTextFill(Color.web("#000000"));
-                label_help.setWrapText(true);
-                
-                canvasBorderPane = new BorderPane();
-                canvasBorderPane.setPadding(new Insets(5));
-                canvasBorderPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
-                canvasBorderPane.setCenter(label_help);
-                
-                border.getChildren().clear();
-                border.setCenter(canvasBorderPane);
-
-                vbox.getChildren().add(border); 
+                vbox_text(vbox, text);
             }
         });
 
@@ -163,6 +84,11 @@ public class Menu extends Parent implements Serializable{
             @Override
             public void handle(ActionEvent actionEvent) {
                 level = load_data();
+                if (level == -1){
+                    String text = "No game has been saved.";
+                    vbox_text(vbox, text);
+                    return;
+                }
                 for (Tower tower: Board.get_instance().get_towers()){
                     Thread thread_tower = new Thread(tower);
                     Game.get_instance().add_thread(thread_tower);
@@ -186,23 +112,23 @@ public class Menu extends Parent implements Serializable{
                 label_level.setFont(Font.font("Cambria", 20));
                 label_level.setTextFill(Color.web("#000000"));
                 menulevels.getChildren().add(label_level);
-                
-                vbox.setVisible(true);
-                level1  = new Button("Level 1");
-                level2  = new Button("Level 2");
-                level3  = new Button("Level 3");
 
-                menulevels.getChildren().addAll(level1,level2,level3);
+                Button level1  = new Button("Level 1");
+                Button level2  = new Button("Level 2");
+                Button level3  = new Button("Level 3");
 
-                BorderPane canvasBorderPane = new BorderPane();
-                canvasBorderPane.setPadding(new Insets(5));
-                canvasBorderPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
-                canvasBorderPane.setCenter(menulevels);
+                menulevels.getChildren().addAll(level1, level2, level3);
 
+                BorderPane bp = new BorderPane();
+                bp.setPadding(new Insets(5));
+                bp.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
+                bp.setCenter(menulevels);
+
+                BorderPane border = new BorderPane();
                 border.getChildren().clear();
-                border.setCenter(canvasBorderPane);
+                border.setCenter(bp);
 
-                //root.getChildren().add(canvasBorderPane);
+                //root.getChildren().add(bp);
                 vbox.getChildren().add(border);
                 
                 level1.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -224,7 +150,7 @@ public class Menu extends Parent implements Serializable{
     }
 
     public void launch_game(){
-        root_play = new Group();
+        Group root_play = new Group();
         //theStage.setScene(new Scene(root2,1920,1080));
         theStage.setScene(new Scene(root_play, theStage.getWidth(), theStage.getHeight()));
         theStage.show();
@@ -263,7 +189,42 @@ public class Menu extends Parent implements Serializable{
             save = (Save) ois.readObject();
             save.init();
             ois.close();
-        } catch (Exception e) {e.printStackTrace();}
+        } catch (Exception e) {
+            return -1;
+        }
         return save.get_level();
+    }
+
+    public void vbox_text(Group vbox, String text){
+        vbox.getChildren().clear();
+        vbox.setLayoutX(400);
+        vbox.setLayoutY(100);
+
+        Label label_help = new Label(text);
+        label_help.setFont(Font.font("Cambria", 20));
+        label_help.setTextFill(Color.web("#000000"));
+        label_help.setWrapText(true);
+
+        BorderPane canvasBorderPane = new BorderPane();
+        canvasBorderPane.setPadding(new Insets(5));
+        canvasBorderPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), Insets.EMPTY)));
+        canvasBorderPane.setCenter(label_help);
+
+        BorderPane border = new BorderPane();
+        border.setCenter(canvasBorderPane);
+        border.setPadding(new Insets(5));
+        border.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(0), Insets.EMPTY)));
+
+        vbox.getChildren().add(border);
+    }
+
+    public Button new_button(String text, int x, int y, int width, int height, String color){
+        Button button = new Button(text);
+        button.setLayoutX(x);
+        button.setLayoutY(y);
+        button.setPrefWidth(width);
+        button.setPrefHeight(height);
+        button.setStyle(color);
+        return button;
     }
 }
